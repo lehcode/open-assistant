2a8e04d0f639c61e1512b4e36b9efb3d466dd41d
lehcode <53556648+lehcode@users.noreply.github.com>
2024-10-27 02:56:54 +0300

    chore: Update NX config

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -1,10 +1,15 @@
+/* eslint-disable no-undef */
+// @ts-nocheck
 const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { join } = require('path');
+const path = require('path');
 
 module.exports = {
   output: {
     path: join(__dirname, '../dist/api'),
   },
+  context: path.resolve(__dirname, 'src'),
+  entry: './src/main.ts',
   plugins: [
     new NxAppWebpackPlugin({
       target: 'node',
