b5daf17315676e3d71263d02b5451edd6f5ea687
lehcode <53556648+lehcode@users.noreply.github.com>
2024-11-14 03:51:10 +0200

    feat: "Admin-e2e" project Eslint configured

--- apps/admin-e2e/eslint.config.cjs
+++ apps/admin-e2e/eslint.config.cjs
@@ -1,13 +1,56 @@
-const config = require("eslint-plugin-playwright");
 const baseConfig = require("../eslint.config.cjs");
+const { FlatCompat } = require("@eslint/eslintrc");
+const eslintJs = require("@eslint/js");
+const { node, jest, vitest } = require("globals");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname, // optional; default: process.cwd()
+  resolvePluginsRelativeTo: __dirname, // optional
+  recommendedConfig: eslintJs.configs.recommended, // optional unless using "eslint:recommended"
+  allConfig: eslintJs.configs.all // optional unless using "eslint:all"
+});
+
+const config = [...baseConfig];
+
+config.plugins = [...compat.plugins("eslint-plugin-playwright")];
+
+config.env = [
+  ...compat.env({
+    es2022: true,
+    node: true
+  })
+];
+
+config.extends = compat.extends(
+  ...["plugin:playwright/recommended", "plugin:playwright/jest-playwright", "plugin:playwright/playwright-test"]
+);
 
 module.exports = [
-  config.configs["flat/recommended"],
-
-  ...baseConfig,
+  ...config,
   {
-    files: ["**/*.ts", "**/*.js"],
+    files: ["**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        globals: {
+          ...node
+        }
+      }
+    },
     // Override or add rules here
-    rules: {},
+    rules: {}
   },
+  {
+    files: ["**/*.spec.ts"],
+    languageOptions: {
+      parserOptions: {
+        globals: {
+          ...node,
+          ...jest,
+          ...vitest
+        }
+      }
+    },
+    // Override or add rules here
+    rules: {}
+  }
 ];
