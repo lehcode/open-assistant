a848e350fbdf56013eb79b9def577c5f38bc3f37
lehcode <53556648+lehcode@users.noreply.github.com>
2024-11-14 03:57:13 +0200

    feat: "Api-e2e" project Eslint configured

--- apps/api-e2e/eslint.config.cjs
+++ apps/api-e2e/eslint.config.cjs
@@ -1,11 +1,8 @@
-const { rules } = require("eslint-plugin-nestjs");
-const baseConfig = require("../eslint.config.cjs");
+const baseConfig = require("../api/eslint.config.cjs");
 
 const config = [...baseConfig];
 
 config.rules = {
-  ...rules,
-  // ...rules["flat/recommended"],
   "@nx/enforce-module-boundaries": "off",
 };
 
